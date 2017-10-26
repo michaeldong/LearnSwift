5283c491bb299086526083509d7155d3f095d353
xingdongdong <xingdongdong@xiaomi.com>
2017-10-26 17:52:58 +0800

    define struct

---
+++ LearnSwift/LearnSwift/LearnSwift/objectMapperTest/MJYPGetList.swift
@@ -0,0 +1,57 @@
+//
+//  MJYPGetList.swift
+//
+//  Created by michaelxing on 2017/10/26
+//  Copyright (c) xiaomi. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+public struct MJYPGetList: Mappable {
+
+  // MARK: Declaration for string constants to be used to decode and also serialize.
+  private struct SerializationKeys {
+    static let descriptionValue = "description"
+    static let data = "data"
+    static let message = "message"
+    static let code = "code"
+  }
+
+  // MARK: Properties
+  public var descriptionValue: String?
+  public var data: MJYPData?
+  public var message: String?
+  public var code: Int?
+
+  // MARK: ObjectMapper Initializers
+  /// Map a JSON object to this class using ObjectMapper.
+  ///
+  /// - parameter map: A mapping from ObjectMapper.
+  public init?(map: Map){
+
+  }
+
+  /// Map a JSON object to this class using ObjectMapper.
+  ///
+  /// - parameter map: A mapping from ObjectMapper.
+  public mutating func mapping(map: Map) {
+    descriptionValue <- map[SerializationKeys.descriptionValue]
+    data <- map[SerializationKeys.data]
+    message <- map[SerializationKeys.message]
+    code <- map[SerializationKeys.code]
+  }
+
+  /// Generates description of the object in the form of a NSDictionary.
+  ///
+  /// - returns: A Key value pair containing all valid values in the object.
+  public func dictionaryRepresentation() -> [String: Any] {
+    var dictionary: [String: Any] = [:]
+    if let value = descriptionValue { dictionary[SerializationKeys.descriptionValue] = value }
+    if let value = data { dictionary[SerializationKeys.data] = value.dictionaryRepresentation() }
+    if let value = message { dictionary[SerializationKeys.message] = value }
+    if let value = code { dictionary[SerializationKeys.code] = value }
+    return dictionary
+  }
+
+}
---
+++ LearnSwift/LearnSwift/LearnSwift/objectMapperTest/MJYPTop.swift
@@ -0,0 +1,45 @@
+//
+//  MJYPTop.swift
+//
+//  Created by michaelxing on 2017/10/26
+//  Copyright (c) xiaomi. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+public struct MJYPTop: Mappable {
+
+  // MARK: Declaration for string constants to be used to decode and also serialize.
+  private struct SerializationKeys {
+
+  }
+
+  // MARK: Properties
+
+
+  // MARK: ObjectMapper Initializers
+  /// Map a JSON object to this class using ObjectMapper.
+  ///
+  /// - parameter map: A mapping from ObjectMapper.
+  public init?(map: Map){
+
+  }
+
+  /// Map a JSON object to this class using ObjectMapper.
+  ///
+  /// - parameter map: A mapping from ObjectMapper.
+  public func mapping(map: Map) {
+
+  }
+
+  /// Generates description of the object in the form of a NSDictionary.
+  ///
+  /// - returns: A Key value pair containing all valid values in the object.
+  public func dictionaryRepresentation() -> [String: Any] {
+    var dictionary: [String: Any] = [:]
+
+    return dictionary
+  }
+
+}
