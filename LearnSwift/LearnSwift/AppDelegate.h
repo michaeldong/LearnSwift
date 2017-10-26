e892439bdaac9b066622f6a2ad8b1dd12883a82b
xingdongdong <xingdongdong@xiaomi.com>
2017-10-26 15:55:37 +0800

    init project

---
+++ LearnSwift/LearnSwift/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LearnSwift
+//
+//  Created by michaelxing on 2017/10/26.
+//  Copyright © 2017年 xiaomi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LearnSwift/LearnSwift/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LearnSwift
+//
+//  Created by michaelxing on 2017/10/26.
+//  Copyright © 2017年 xiaomi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
