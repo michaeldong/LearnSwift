ceb239c830e1ff3aedad132f4530b451c48d7115
xingdongdong <xingdongdong@xiaomi.com>
2017-10-27 12:00:48 +0800

    unfinished

--- LearnSwift/viewcontroller/YPTasteViewController.swift
+++ LearnSwift/viewcontroller/YPTasteViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class YPTasteViewController: UIViewController  {
     var goodsTableView : UITableView?
@@ -37,7 +38,22 @@ class YPTasteViewController: UIViewController  {
     }
     
     func fetchData() {
-        goodsData.add("michael")
+//        goodsData.add("michael")
+        
+//MARK-- todo parser json response
+        Alamofire.request("https://httpbin.org/get").responseJSON { response in
+            print("Request: \(String(describing: response.request))")   // original url request
+            print("Response: \(String(describing: response.response))") // http url response
+            print("Result: \(response.result)")                         // response serialization result
+            
+            if let json = response.result.value {
+                print("JSON: \(json)") // serialized json response
+            }
+            
+            if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
+                print("Data: \(utf8Text)") // original server data as UTF8 string
+            }
+        }
     }
 }
 
